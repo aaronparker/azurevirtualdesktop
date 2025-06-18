c793c7daedd542a05debab0a2a4af9571542e696
Aaron Parker <aaron@stealthpuppy.com>
2025-06-18 23:45:10 +1000

    Create deploy.ps1

---
+++ template-spec/network/deploy.ps1
@@ -0,0 +1,37 @@
+$Location = "australiaeast"
+$Date = (Get-Date).ToString("yyyy-MM-dd HH:mm:ss")
+$Tags = @{
+    Application    = "Azure Virtual Desktop"
+    LastUpdateBy   = "aaronparker@cloud.stealthpuppy.com"
+    LastUpdateDate = $Date
+    Criticality    = "Medium"
+    Environment    = "Production"
+    Function       = "Create a virtual network"
+}
+
+$params = @{
+    ResourceGroupName = "rg-Dev-TemplateSpecs-aue"
+    Name              = "New-VirtualNetwork"
+    DisplayName       = "Create a new virtual network"
+    Description       = "Create a new virtual network with subnets, an NSG and a NAT gateway to support Azure Virtual Desktop and Windows 365 workloads."
+    Version           = "1.0"
+    Location          = $Location
+    TemplateFile      = $(Get-ChildItem -Path $PWD -Recurse -Include "main.bicep").FullName
+    # UIFormDefinitionFile = $(Get-ChildItem -Path $PWD -Recurse -Include uiFormDefinition.json).FullName
+    Tag               = $Tags
+    Force             = $true
+    Verbose           = $true
+}
+New-AzTemplateSpec @params
+
+# az ts create `
+#     --resource-group "rg-Dev-TemplateSpecs-aue" `
+#     --name "WindowsServer-vm" `
+#     --display-name "Windows Server 2022" `
+#     --description "Windows Server 2022 virtual machine for infrastructure workloads." `
+#     --version "1.2.6" `
+#     --location "$LOCATION" `
+#     --template-file ./main.bicep `
+#     --ui-form-definition ./uiFormDefinition.json `
+#     --tags Application="Windows Server" LastUpdateBy=$UPN LastUpdateDate=$Date Criticality='Medium' Environment='Lab' Function='Virtual machine template' `
+#     --yes
