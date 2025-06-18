df741377437dac970a27e6978958fcd180fc6d8c
Aaron Parker <aaron@stealthpuppy.com>
2025-06-19 08:46:14 +1000

    Update form

--- template-spec/network/deploy.ps1
+++ template-spec/network/deploy.ps1
@@ -10,17 +10,17 @@ $Tags = @{
 }
 
 $params = @{
-    ResourceGroupName = "rg-Dev-TemplateSpecs-aue"
-    Name              = "New-VirtualNetwork"
-    DisplayName       = "Create a new virtual network"
-    Description       = "Create a new virtual network with subnets, an NSG and a NAT gateway to support Azure Virtual Desktop and Windows 365 workloads."
-    Version           = "1.0"
-    Location          = $Location
-    TemplateFile      = $(Get-ChildItem -Path $PWD -Recurse -Include "main.bicep").FullName
-    # UIFormDefinitionFile = $(Get-ChildItem -Path $PWD -Recurse -Include uiFormDefinition.json).FullName
-    Tag               = $Tags
-    Force             = $true
-    Verbose           = $true
+    ResourceGroupName    = "rg-Dev-TemplateSpecs-aue"
+    Name                 = "New-VirtualNetwork"
+    DisplayName          = "Create a new virtual network"
+    Description          = "Create a virtual network with subnets, an NSG and a NAT gateway to support AVD and Windows 365."
+    Version              = "1.3"
+    Location             = $Location
+    TemplateFile         = $(Get-ChildItem -Path $PWD -Recurse -Include "main.bicep").FullName
+    UIFormDefinitionFile = $(Get-ChildItem -Path $PWD -Recurse -Include uiFormDefinition.json).FullName
+    Tag                  = $Tags
+    Force                = $true
+    Verbose              = $true
 }
 New-AzTemplateSpec @params
 
